5cea84861b6b8231ffc41d90bd8e8420f5669cb8
RAPTERCAR <rileywoodruff03@gmail.com>
2024-11-25 21:40:24 -0600

    minor changes
    very little has been done

--- client/client.py
+++ client/client.py
@@ -1,14 +1,42 @@
 import socket
 import random
-from threading import Thread
+from threading import Thread, Lock
 import pickle
+
 
 host = '127.0.0.1'
 multPorts = [15002,15003,15004,15005,15006,15007,15008,15009,150010,15011]
-class Conn: #not sure whether to handle socketes as a class instance or through threads
+mutex = Lock()
+mutex2 = Lock()
+threads = []
 
-    def __init__(self,) -> None:
-        pass
+# holds aggregate array as well as the next row and collumn to multiply
+class Tracker:
+
+    def __init__(self, size):
+        self.r = 0
+        self.c = 0
+        self.size = size
+        self.done = 0 
+        self.aggArray = [[0]*size for _ in range(size)]
+
+    def increment(self,):
+        x = [self.c,self.r]
+        if(self.c == self.size -1 and self.r == self.size -1):
+            self.finished = 1
+        elif(self.c == self.size - 1):
+            self.c = 0
+            self.r += 1
+        else:
+            self.c += 1
+        return x
+    
+    def place(self, prod, row, collumn):
+        self.aggArray[row][collumn] = prod
+
+    def getArr(self):
+        return self.aggArray
+
     
 def main():
     choice = int(input("What size matrices would you like to multiply: "))
@@ -16,24 +44,46 @@ def main():
     #client_socket.send(choice.encode('utf-8')) 
     matrix1 = generateMatrix(choice)
     matrix2 = generateMatrix(choice)
+    track = Tracker(choice)
     print("\n".join([" ".join(map(str, row)) for row in matrix1]))
     print("\n")
     print("\n".join([" ".join(map(str, row)) for row in matrix2]))
-    arr = getCollumn(matrix1,0,choice)
-    print(arr)
+    print(matrix1[0])
+    #arr = getCollumn(matrix1,0,choice)
+    #print(arr)
+    for i in range(5):
+        server_thread = Thread(target=connect, args=(host, multPorts[i]))
+        threads.append(server_thread)
+        server_thread.start()
 
+    for thread in threads:
+        thread.join()
     #client_socket = connect(host,multPorts[0])
     #data = pickle.dumps(matrix1[0]) #changes array to a form that can be sent over sockets
     #client_socket.send(data)
+
     
     
-def connect(host, port):
+def connect(host, port, m1, m2, tracker):
     try:
         # Attempt to create a socket and connect to the server
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect((host, port))
         print(f"Connected to server at {host}:{port}")
-        return client_socket  # Return the connected socket
+        #return client_socket  # Return the connected socket
+        while(tracker.done == 0):
+            mutex.acquire(1)
+            x = tracker.increment()
+            mutex.release
+            row = pickle.dumps(m1[x[0]])
+            collumn = pickle.dumps(getCollumn(m2,x[1]))
+            client_socket.send(row)
+            client_socket.send(collumn)
+            prod = int(client_socket.recv(1024).decode('utf-8'))
+            mutex2.acquire(1)
+            tracker.place(prod,x[0],x[1])
+            mutex2.release
+        return
     except (ConnectionRefusedError, socket.error):
         # If connection fails, print error and try the next server
         print(f"Server {host}:{port} unavailable. Trying next server...")
@@ -45,9 +95,9 @@ def generateMatrix(size):
             matrix[i][j] =random.randint(0, 9)
     return matrix
 #gets collumn of matrix
-def getCollumn(matrix,collumn, size):
-    arr = [0] * size 
-    for i in range(size):
+def getCollumn(matrix,collumn):
+    arr = [0] * len(matrix[0]) 
+    for i in range(len(matrix[0])):
         arr[i] = matrix[i][collumn]
     return arr
 
